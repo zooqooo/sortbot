989e073056572562e1d3c886364fd643ea86a588
zooqooo <scottmattj@gmail.com>
2019-06-28 23:07:51 -0400

    Commented errors for now

--- sortbot.py
+++ sortbot.py
@@ -7,10 +7,10 @@ domain = 'https://api.noopschallenge.com'
 def print_sep(): print('----------------------------------------------------------------------')
 
 def main():
-    question_url = '/fizzbot'
+    #question_url = '/fizzbot'
     print_sep()
     print("👋 Hello from Sortbot 2000\n\n")
 
-    answer = input("What is your GitHub username?\n\n")
+    #answer = input("What is your GitHub username?\n\n")
 
 if __name__ == '__main__': main()
