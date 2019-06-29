e549db99a2bfe644f0ba4b65a685b7145d5eccb0
zooqooo <scottmattj@gmail.com>
2019-06-28 23:07:06 -0400

    Created python file (unfinished)

---
+++ sortbot.py
@@ -0,0 +1,16 @@
+import json
+import urllib.request
+import urllib.error
+
+domain = 'https://api.noopschallenge.com'
+
+def print_sep(): print('----------------------------------------------------------------------')
+
+def main():
+    question_url = '/fizzbot'
+    print_sep()
+    print("👋 Hello from Sortbot 2000\n\n")
+
+    answer = input("What is your GitHub username?\n\n")
+
+if __name__ == '__main__': main()
